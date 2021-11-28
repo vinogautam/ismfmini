f68d9e3dbf52a583843a8d0c84bf8ff04aa27e8a
vinogautam <dhanavel237vino@gmail.com>
2021-11-28 19:01:01 +0530

    Update dashboard.js

--- scripts/controllers/dashboard.js
+++ scripts/controllers/dashboard.js
@@ -90,7 +90,7 @@ angular.module('yapp')
     
     $scope.transactions = {};
     
-    firebase.database().ref('transaction').limitToLast(50).on('value', function(snap) {
+    firebase.database().ref('transaction').limitToLast(100).on('value', function(snap) {
         $scope.transactions = snap.val();
         $scope.$apply();
     });
